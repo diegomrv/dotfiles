7e109a354cdd8bc9703980e41ecddaf91e61a6df
Diego Rodriguez <diego@strangepixels.co>
2024-07-11 13:40:57 -0600

    MacOS zsh config

---
+++ zsh/macos.zsh
@@ -0,0 +1,15 @@
+# Separate ZSH configuration for MacOS
+export PATH="$PATH:$HOME/.composer/vendor/bin"
+
+# bun completions
+[ -s "/Users/wolfius/.bun/_bun" ] && source "/Users/wolfius/.bun/_bun"
+
+# bun
+export BUN_INSTALL="$HOME/.bun"
+export PATH="$BUN_INSTALL/bin:$PATH"
+
+
+if [ "$TERM_PROGRAM" != "Apple_Terminal" ]; then
+  #eval "$(oh-my-posh init zsh --config ~/.M365Princess-mod.omp.json)"
+  eval "$(oh-my-posh init zsh --config ~/.hunk-mod.omp.json)"
+fi
